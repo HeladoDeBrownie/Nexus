d8192134e756c047cb24173fd3d4cc8d0c5f8b43
helado de brownie <helado.de.brownie@gmail.com>
2020-09-11 01:46:44 -0400

    implement rudimentary text renderer

---
+++ Font.lua
@@ -0,0 +1,68 @@
+local Font = {}
+local private = setmetatable({}, {__mode = 'k'})
+local font_metatable = {__index = Font}
+local lg = love.graphics
+
+function Font.new(metadata)
+    local result = setmetatable({}, font_metatable)
+    local characters = metadata.characters
+    local columns, rows = metadata.columns, metadata.rows
+    local glyph_width = metadata.glyph_width
+    local glyph_height = metadata.glyph_height
+    local image = lg.newImage(metadata.file_name)
+    local image_width, image_height = image:getDimensions()
+    local quads = {}
+    local column, row = 1, 0
+
+    for character in characters:gmatch'.' do
+        quads[character] =
+            lg.newQuad(
+                column * glyph_width,
+                row * glyph_height,
+                glyph_width,
+                glyph_height,
+                image_width,
+                image_height
+            )
+
+        if column < columns - 1 then
+            column = column + 1
+        elseif row < rows - 1 then
+            column = 0
+            row = row + 1
+        else
+            error'Font.new: inconsistent size specification'
+        end
+    end
+
+    private[result] = {
+        love_image = image,
+
+        love_missing_glyph_quad = lg.newQuad(
+            0, 0, glyph_width, glyph_height, image_width, image_height
+        ),
+
+        love_quads = quads,
+        metadata = metadata,
+    }
+
+    return result
+end
+
+function Font:print(text)
+    local self_ = private[self]
+    local x, y = 0, 0
+
+    for character in text:gmatch'.' do
+        local quad = self_.love_quads[character]
+
+        if quad == nil then
+            quad = self_.love_missing_glyph_quad
+        end
+
+        lg.draw(self_.love_image, quad, x, y)
+        x = x + self_.metadata.glyph_width
+    end
+end
+
+return Font
---
+++ main.lua
@@ -0,0 +1,5 @@
+local font = require'Font'.new(require'Assets/Font')
+
+function love.draw()
+    font:print'uwu hewwoOO!! Wat dis??'
+end
