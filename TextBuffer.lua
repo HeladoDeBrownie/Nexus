8dfc5ff0f2ac75a746aa8808d8cf00d22de09e3b
helado de brownie <helado.de.brownie@gmail.com>
2020-09-13 01:11:13 -0400

    add rudimentary text entry

---
+++ TextBuffer.lua
@@ -0,0 +1,33 @@
+local utf8 = require'utf8'
+
+local TextBuffer = {}
+local private = setmetatable({}, {__mode = 'k'})
+local text_buffer_metatable = {__index = TextBuffer}
+
+function TextBuffer.new()
+    local result = setmetatable({}, text_buffer_metatable)
+
+    private[result] = {
+        text = '',
+    }
+
+    return result
+end
+
+function TextBuffer:read()
+    return private[self].text
+end
+
+function TextBuffer:append(text)
+    private[self].text = private[self].text .. text
+end
+
+function TextBuffer:backspace()
+    local text = private[self].text
+
+    if utf8.len(text) > 0 then
+        private[self].text = text:sub(1, utf8.offset(text, -1) - 1)
+    end
+end
+
+return TextBuffer
--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ local lg = love.graphics
 lg.setDefaultFilter('nearest', 'nearest')
 local font = require'Font'.new(require'Assets/Font')
 local shader = lg.newShader'pixel_shader.glsl'
+local buffer = require'TextBuffer'.new()
 
 shader:sendColor('palette',
     {243 / 255, 243 / 255, 243 / 255, 1},
@@ -18,9 +19,16 @@ function love.draw()
     lg.rectangle('fill', 0, 0, lg.getDimensions())
     lg.setColor(1, 1, 1)
     lg.scale(8)
-    font:print[[
-uwu hewwoOO!! Wat dis??
-he—hewwoOOOOOOOO?????!?!?!?
--–—]]
+    font:print('type here: ' .. buffer:read())
     lg.pop()
+end
+
+function love.textinput(text)
+    buffer:append(text)
+end
+
+function love.keypressed(key)
+    if key == 'backspace' then
+        buffer:backspace()
+    end
 end
