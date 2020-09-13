3d20f69f0e85940649e2b053d4d36aebd31af246
helado de brownie <helado.de.brownie@gmail.com>
2020-09-13 13:48:40 -0400

    rearrange main.lua; divide into sections

--- main.lua
+++ main.lua
@@ -1,21 +1,48 @@
+-- # Modules
+
 local lg = love.graphics
-lg.setDefaultFilter('nearest', 'nearest')
-local font = require'Font'.new(require'Assets/Font')
-local shader = lg.newShader'pixel_shader.glsl'
-local buffer = require'TextBuffer'.new()
+local Font = require'Font'
+local TextBuffer = require'TextBuffer'
+
+-- # State
+
+local buffer
+local font
+local shader
+
+-- # Helpers
 
 local function rgb24_to_love_color(red, green, blue)
     return red / 255, green / 255, blue / 255, 1
 end
 
-shader:sendColor('palette',
-    {rgb24_to_love_color(243, 243, 243)},
-    {rgb24_to_love_color(  0, 228,  54)},
-    {rgb24_to_love_color(  0, 135,  81)},
-    {rgb24_to_love_color( 95,  87,  79)}
-)
+-- # Callbacks
 
-lg.setShader(shader)
+function love.load()
+    lg.setDefaultFilter('nearest', 'nearest')
+    buffer = TextBuffer.new()
+    font = Font.new(require'Assets/Font')
+    shader = lg.newShader'pixel_shader.glsl'
+
+    shader:sendColor('palette',
+        {rgb24_to_love_color(243, 243, 243)},
+        {rgb24_to_love_color(  0, 228,  54)},
+        {rgb24_to_love_color(  0, 135,  81)},
+        {rgb24_to_love_color( 95,  87,  79)}
+    )
+
+    lg.setShader(shader)
+end
+
+function love.keypressed(key)
+    if key == 'backspace' then
+        buffer:backspace()
+    end
+end
+
+function love.textinput(text)
+    buffer:append(text)
+end
 
 function love.draw()
     lg.push'all'
@@ -25,14 +52,4 @@ function love.draw()
     lg.scale(8)
     font:print('type here: ' .. buffer:read())
     lg.pop()
-end
-
-function love.textinput(text)
-    buffer:append(text)
-end
-
-function love.keypressed(key)
-    if key == 'backspace' then
-        buffer:backspace()
-    end
 end
