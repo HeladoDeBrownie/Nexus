3db87c4646d3545f13c5c6fa9ed142100fdc77a2
helado de brownie <helado.de.brownie@gmail.com>
2020-09-14 01:13:41 -0400

    add some more programming glyphs

--- Assets/Font.lua
+++ Assets/Font.lua
@@ -1,7 +1,7 @@
 return {
     file_name = 'Assets/Font.png',
     characters = [[
-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ,.!?:;-–—]],
-    columns = 26, rows = 3,
+ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ,.!?:;-–—+=~"'()[]<>{}]],
+    columns = 26, rows = 4,
     glyph_width = 8, glyph_height = 12,
 }
