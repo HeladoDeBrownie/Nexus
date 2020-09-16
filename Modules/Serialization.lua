fd441addf67b0e137f84ce6589edfa58ec6a06d6
helado de brownie <helado.de.brownie@gmail.com>
2020-09-16 15:59:28 -0400

    fill in serialization logic

--- Modules/Serialization.lua
+++ Modules/Serialization.lua
@@ -1,12 +1,25 @@
 local Serialization = {}
 
--- TODO: stub
-function Serialization.serialize_table(table)
-    return [[
+local RETURN_STRING = [[
 return {
-    global_scale = 2,
+%s
 }
 ]]
+
+local ITEM_STRING = [[
+    %s = %s,]]
+
+-- TODO: stub
+function Serialization.serialize_table(a_table)
+    local string_table = {}
+
+    for key, value in pairs(a_table) do
+        if type(key) == 'string' then
+            table.insert(string_table, ITEM_STRING:format(key, value))
+        end
+    end
+
+    return RETURN_STRING:format(table.concat(string_table, '\n'))
 end
 
 return Serialization
