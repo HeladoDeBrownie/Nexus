ddf02228a91d5035e062bad644eb37ca6a807771
helado de brownie <helado.de.brownie@gmail.com>
2020-09-21 18:34:55 -0400

    reorganize, remove redundancies from, and add comments to main.lua

--- main.lua
+++ main.lua
@@ -1,73 +1,61 @@
--- Use nearest neighbor scaling in order to preserve pixel fidelity.
-love.graphics.setDefaultFilter('nearest', 'nearest')
-
--- # Modules
-
--- Look for modules in the Modules directory.
-package.path = './Modules/?.lua;' .. package.path
-
-local lg = love.graphics
-local lk = love.keyboard
-local Serialization = require'Serialization'
-local Settings = require'Settings'
-local UI = require'UI'
+--[[
+    This module is the LÖVE main entry point, evaluated after conf.lua. While
+    all the LÖVE callbacks are defined here, most of the actual application
+    logic happens in the UI modules (Modules/UI/*).
+--]]
 
 -- # State
 
+-- Almost all state is handled in widget code.
 local main_widget
-local shader
 
 -- # Helpers
 
 local function is_ctrl_down()
-    return lk.isDown'lctrl' or lk.isDown'rctrl'
-end
-
-local function rgb24_to_love_color(red, green, blue)
-    return red / 255, green / 255, blue / 255, 1
+    return love.keyboard.isDown'lctrl' or love.keyboard.isDown'rctrl'
 end
 
 -- # Callbacks
 
 function love.load()
-    lk.setKeyRepeat(true)
+    -- Look for modules in the Modules directory.
+    package.path = './Modules/?.lua;' .. package.path
 
+    --[[
+        Use nearest neighbor scaling in order to preserve pixel fidelity. Do
+        this before loading any modules so that the setting is in place when
+        images are loaded.
+    --]]
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- While a key is held, repeat its key event after a short delay.
+    love.keyboard.setKeyRepeat(true)
+
+    local UI = require'UI'
     main_widget = UI.Overlay.new(UI.TileView.new(), UI.Console.new'> ')
+end
 
-    -- Set up the palette swap pixel shader.
-
-    shader = lg.newShader'palette_swap.glsl'
-
-    -- Tell the shader which colors to swap in.
-    shader:sendColor('palette',
-        -- For now, these are just some sample PICO-8 colors.
-        {rgb24_to_love_color(243, 243, 243)},
-        {rgb24_to_love_color(  0, 228,  54)},
-        {rgb24_to_love_color(  0, 135,  81)},
-        {rgb24_to_love_color( 95,  87,  79)}
+function love.quit()
+    -- Write any changes to the settings to the save directory.
+    love.filesystem.write('Settings.lua',
+        require'Serialization'.to_lua_module(require'Settings')
     )
+end
 
-    lg.setShader(shader)
+-- The remaining callbacks defined here are thin wrappers around widget code.
+
+function love.draw()
+    main_widget:draw(0, 0, lg.getDimensions())
 end
 
 function love.keypressed(key)
     main_widget:on_key(key, is_ctrl_down())
 end
 
-function love.wheelmoved(_, y)
-    main_widget:on_scroll(y, is_ctrl_down())
-end
-
 function love.textinput(text)
     main_widget:on_text_input(text)
 end
 
-function love.draw()
-    main_widget:draw(0, 0, lg.getDimensions())
-end
-
-function love.quit()
-    love.filesystem.write('Settings.lua',
-        Serialization.to_lua_module(Settings)
-    )
+function love.wheelmoved(_, y)
+    main_widget:on_scroll(y, is_ctrl_down())
 end
