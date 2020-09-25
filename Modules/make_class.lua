e6388975f010b1a2a13eb643e3c9f547f85948ba
helado de brownie <helado.de.brownie@gmail.com>
2020-09-24 20:47:51 -0400

    make superclasses actually work

--- Modules/make_class.lua
+++ Modules/make_class.lua
@@ -8,18 +8,31 @@ return function (properties)
     local superclass = properties.superclass
     local class = setmetatable({}, {__index = superclass})
     local member_metatable = {__index = class}
+    private[class] = properties
 
     function class.new(...)
-        local self = setmetatable({}, member_metatable)
-        local self_ = setmetatable({}, {__index = self})
+        local self, self_
+
+        self = setmetatable({}, member_metatable)
+        self_ = setmetatable({}, {__index = self})
         private[self] = self_
-        new(self_, ...)
+
+        local function super(...)
+            private[superclass].new(self_, ...)
+        end
+
+        if superclass == nil then
+            new(self_, ...)
+        else
+            new(self_, super, ...)
+        end
+
         return self
     end
 
     for method_name, method in pairs(methods) do
         class[method_name] = function (self, ...)
-            return method(private[self], ...)
+            return method(private[self] or self, ...)
         end
     end
 
