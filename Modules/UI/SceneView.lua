b0a58e904a79fb1fc6c10c3016ab24266ab83b77
helado de brownie <helado.de.brownie@gmail.com>
2020-10-01 01:46:51 -0400

    use different palettes for tiles and entities

--- Modules/UI/SceneView.lua
+++ Modules/UI/SceneView.lua
@@ -22,7 +22,18 @@ function SceneView:initialize(scene)
     Widget.initialize(self)
     Scalable.initialize(self, require'Settings'.UI.SceneView)
     self.scene = scene
+end
 
+function SceneView:apply_tile_palette()
+    self:set_palette(
+        {rgb24_to_love_color(  0,   0,   0)},
+        {rgb24_to_love_color( 50,  50,  50)},
+        {rgb24_to_love_color(150, 150, 150)},
+        {rgb24_to_love_color(250, 250, 250)}
+    )
+end
+
+function SceneView:apply_entity_palette()
     self:set_palette(
         {rgb24_to_love_color(243, 243, 243)},
         {rgb24_to_love_color(  0, 228,  54)},
@@ -47,7 +58,9 @@ function SceneView:draw_widget(x, y, width, height)
         math.floor(base_y - player_sy - 6)
     )
 
+    self:apply_tile_palette()
     love.graphics.draw(self.scene:get_chunk(0, 0))
+    self:apply_entity_palette()
     love.graphics.draw(sprite, player_sx, player_sy)
     love.graphics.draw(sprite2, x + 24, y + 36)
 end
