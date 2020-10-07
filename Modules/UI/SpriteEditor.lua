dc22f49508cead461f58e3253e174b00ef8acbe6
helado de brownie <helado.de.brownie@gmail.com>
2020-10-07 02:02:20 -0400

    display pixel grid

--- Modules/UI/SpriteEditor.lua
+++ Modules/UI/SpriteEditor.lua
@@ -9,6 +9,13 @@ local Widget = require'UI/Widget'
 local SPRITE_WIDTH = 12
 local SPRITE_HEIGHT = 12
 
+local GRAYSCALE_PALETTE = {
+    [0] = {0, 0, 0, 0},
+    [1] = {0, 0, 0, 1},
+    [2] = {0.5, 0.5, 0.5, 1},
+    [3] = {1, 1, 1, 1},
+}
+
 --# Interface
 
 function SpriteEditor:initialize()
@@ -19,13 +26,33 @@ function SpriteEditor:initialize()
         self.pixels[x] = {}
 
         for y = 1, SPRITE_HEIGHT do
-            self.pixels[x][y] = 0
+            self.pixels[x][y] = 2
         end
     end
+
+    self.pixels[6][6] = 3
+
+    self:set_palette(
+        {0, 0, 0, 1},
+        {0.1, 0.1, 0.1, 1},
+        {0.3, 0.3, 0.3, 1},
+        {0.6, 0.6, 0.6, 1}
+    )
 end
 
 function SpriteEditor:draw_widget(x, y, width, height)
-    print'TODO'
+    for x = 1, SPRITE_WIDTH do
+        for y = 1, SPRITE_HEIGHT do
+            local x_increment = width / SPRITE_WIDTH
+            local y_increment = height / SPRITE_HEIGHT
+            love.graphics.setColor(GRAYSCALE_PALETTE[self.pixels[x][y]])
+
+            love.graphics.rectangle('fill',
+                (x - 1) * x_increment, (y - 1) * y_increment,
+                x_increment, y_increment
+            )
+        end
+    end
 end
 
 return augment(mix{Widget, SpriteEditor})
