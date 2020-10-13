2f8eda9aec0b041f5b7363c09c7902e1f104fba4
helado de brownie <helado.de.brownie@gmail.com>
2020-10-12 21:33:04 -0400

    implement to_normalized_rgba

--- Modules/Color.lua
+++ Modules/Color.lua
@@ -26,8 +26,33 @@ function Color:initialize(hue, saturation, value)
 end
 
 function Color:to_normalized_rgba()
-    -- TODO
-    return 0, 0, 0, 1
+    -- Algorithm source: https://en.wikipedia.org/wiki/HSL_and_HSV#HSV_to_RGB
+
+    local normalized_value = self.value / VALUE_MAX
+    local normalized_saturation = self.saturation / SATURATION_MAX
+    local chroma = normalized_value * normalized_saturation
+    local hue_ = self.hue / (HUE_CAP / 6)
+    local x = chroma * (1 - math.abs(hue_ % 2 - 1))
+    local r_, g_, b_
+
+    if 0 <= hue_ and hue_ <= 1 then
+        r_, g_, b_ = chroma, x, 0
+    elseif 1 < hue_ and hue_ <= 2 then
+        r_, g_, b_ = x, chroma, 0
+    elseif 2 < hue_ and hue_ <= 3 then
+        r_, g_, b_ = 0, chroma, x
+    elseif 3 < hue_ and hue_ <= 4 then
+        r_, g_, b_ = 0, x, chroma
+    elseif 4 < hue_ and hue_ <= 5 then
+        r_, g_, b_ = x, 0, chroma
+    elseif 5 < hue_ and hue_ <= 6 then
+        r_, g_, b_ = chroma, 0, hue
+    else
+        r_, g_, b_ = 0, 0, 0
+    end
+
+    local base = normalized_value - chroma
+    return r_ + base, g_ + base, b_ + base, 1
 end
 
 --#
