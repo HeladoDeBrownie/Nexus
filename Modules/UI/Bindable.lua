b81d7adcf7c77dbcb04288779cffc6e373eb570a
helado de brownie <helado.de.brownie@gmail.com>
2020-10-15 11:27:25 -0400

    separate key binding logic into new mixin

---
+++ Modules/UI/Bindable.lua
@@ -0,0 +1,58 @@
+local Bindable = {}
+
+--# Requires
+
+local utf8 = require'utf8'
+local is_ctrl_down = require'Helpers'.is_ctrl_down
+
+--# Helpers
+
+local function key_combination_string(key, ctrl_down)
+    local result = key:gsub(utf8.charpattern, string.upper, 1)
+
+    if ctrl_down then
+        result = 'Ctrl+' .. result
+    end
+
+    return result
+end
+
+--# Interface
+
+function Bindable:initialize()
+    self.bindings = {}
+end
+
+-- Create a key binding, used by Bindable.on_key to handle key combinations.
+function Bindable:bind(key_combination, handler, extra_data)
+    self.bindings[key_combination] = {
+        handler = handler,
+        extra_data = extra_data,
+    }
+end
+
+function Bindable:on_key(key, down)
+    -- When a key combination is pressed, trigger a binding if there is an
+    -- appropriate one. Otherwise, call the fallback key handler.
+    if down then
+        local key_combination = key_combination_string(key, is_ctrl_down())
+        local binding = self.bindings[key_combination]
+
+        if binding == nil then
+            return self:on_unbound_key(key, down)
+        else
+            return binding.handler(self, binding.extra_data)
+        end
+    else
+        return self:on_unbound_key(key, down)
+    end
+end
+
+--## Abstract
+
+-- Called when a key combination is not handled by a binding.
+function Bindable:on_unbound_key(key, down) end
+
+--#
+
+return Bindable
--- Modules/UI/Widget.lua
+++ Modules/UI/Widget.lua
@@ -7,25 +7,12 @@ local Widget = {}
 
 --# Requires
 
-local utf8 = require'utf8'
-local is_ctrl_down = require'Helpers'.is_ctrl_down
-
---# Helpers
-
-local function key_combination_string(key, ctrl_down)
-    local result = key:gsub(utf8.charpattern, string.upper, 1)
-
-    if ctrl_down then
-        result = 'Ctrl+' .. result
-    end
-
-    return result
-end
+local Bindable = require'UI/Bindable'
 
 --# Interface
 
 function Widget:initialize(color_scheme)
-    self.bindings = {}
+    Bindable.initialize(self)
     self.canvas = love.graphics.newCanvas()
     self.color_scheme = color_scheme
     self.shader = love.graphics.newShader'palette_swap.glsl'
@@ -52,35 +39,6 @@ function Widget:resize(width, height)
     -- discard the old canvas and create a new one of the correct size.
     self.canvas = love.graphics.newCanvas(width, height)
 end
-
---## Input
-
--- Create a key binding, used by Widget.on_key to handle key combinations.
-function Widget:bind(key_combination, handler, extra_data)
-    self.bindings[key_combination] = {
-        handler = handler,
-        extra_data = extra_data,
-    }
-end
-
-function Widget:on_key(key, down)
-    -- When a key combination is pressed, trigger a binding if there is an
-    -- appropriate one. Otherwise, call the fallback key handler.
-    if down then
-        local key_combination = key_combination_string(key, is_ctrl_down())
-        local binding = self.bindings[key_combination]
-
-        if binding == nil then
-            return self:on_unbound_key(key, down)
-        else
-            return binding.handler(self, binding.extra_data)
-        end
-    else
-        return self:on_unbound_key(key, down)
-    end
-end
-
---## Drawing
 
 -- Draw the widget to its canvas. This does *not* draw it to the screen. For
 -- screen drawing, use love.graphics.draw with Widget.get_canvas.
@@ -122,12 +80,9 @@ function Widget:on_scroll(units, ctrl) end
 -- Called when text is entered.
 function Widget:on_text_input(text) end
 
--- Called when a key combination is not handled by a binding.
-function Widget:on_unbound_key(key, down) end
-
 -- Called each frame.
 function Widget:tick() end
 
 --#
 
-return Widget
+return mix{Bindable, Widget}
