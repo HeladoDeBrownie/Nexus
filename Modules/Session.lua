656b9a01e5a87d2836479ed5c4856ec97eabcb50
helado de brownie <helado.de.brownie@gmail.com>
2020-10-26 03:29:17 -0400

    only remove them if they actually are closed

--- Modules/Session.lua
+++ Modules/Session.lua
@@ -12,7 +12,7 @@ local DEFAULT_HOST = 'localhost'
 -- (63987), but that would have placed it in the ephemeral ports range.
 local DEFAULT_PORT = 43569
 
-local MAXIMUM_PLAYERS = 32
+local MAXIMUM_PLAYERS = 1
 
 --# Interface
 
@@ -85,9 +85,9 @@ function Session:process()
             local visitor = self.slots[slot_id]
 
             if visitor ~= nil then
-                local result = visitor:send(tostring(random_number) .. '\n')
+                local result, reason = visitor:send(tostring(random_number) .. '\n')
 
-                if result == nil then
+                if result == nil and reason == 'closed' then
                     self.slots[slot_id] = nil
                 end
             end
