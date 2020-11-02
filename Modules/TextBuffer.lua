ca655f3c72fd155b30f82344473ddae8086eaa97
helado de brownie <helado.de.brownie@gmail.com>
2020-11-02 01:54:32 -0500

    refactor TextBuffer in preparation for scrollback limit

--- Modules/TextBuffer.lua
+++ Modules/TextBuffer.lua
@@ -11,23 +11,45 @@ function TextBuffer:initialize()
 end
 
 function TextBuffer:read()
-    return self.text
+    local joined_text = ''
+
+    for line_number, line in ipairs(self.lines) do
+        if line_number > 1 then
+            joined_text = joined_text .. '\n'
+        end
+
+        joined_text = joined_text .. line
+    end
+
+    return joined_text
 end
 
 function TextBuffer:append(text)
-    self.text = self.text .. text
+    for line in (text .. '\n'):gmatch'([^\n]*)\n' do
+        self.lines[self.last_line_number] =
+            self.lines[self.last_line_number] .. line
+
+        self.last_line_number = self.last_line_number + 1
+        self.lines[self.last_line_number] = ''
+    end
+
+    self:backspace()
 end
 
 function TextBuffer:backspace()
-    local text = self.text
+    local line = self.lines[self.last_line_number]
 
-    if utf8.len(text) > 0 then
-        self.text = text:sub(1, utf8.offset(text, -1) - 1)
+    if utf8.len(line) > 0 then
+        self.lines[self.last_line_number] = line:sub(1, utf8.offset(line, -1) - 1)
+    elseif self.last_line_number > 1 then
+        self.lines[self.last_line_number] = nil
+        self.last_line_number = self.last_line_number - 1
     end
 end
 
 function TextBuffer:clear()
-    self.text = ''
+    self.lines = {''}
+    self.last_line_number = 1
 end
 
 return augment(TextBuffer)
