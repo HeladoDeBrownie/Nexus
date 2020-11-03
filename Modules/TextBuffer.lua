fe3a2b9d25092d5dd6b0d72ae3ad535eea5c95ac
helado de brownie <helado.de.brownie@gmail.com>
2020-11-03 00:40:18 -0500

    implement text buffer scrollback limit

--- Modules/TextBuffer.lua
+++ Modules/TextBuffer.lua
@@ -2,54 +2,57 @@ local TextBuffer = {}
 
 --# Requires
 
+local RingBuffer = require'RingBuffer'
 local utf8 = require'utf8'
+
+--# Constants
+
+TextBuffer.scrollback_limit = 1000
 
 --# Interface
 
-function TextBuffer:initialize()
+function TextBuffer:initialize(scrollback_limit)
+    self.scrollback_limit = scrollback_limit
     self:clear()
 end
 
 function TextBuffer:read()
     local joined_text = ''
 
-    for line_number, line in ipairs(self.lines) do
-        if line_number > 1 then
-            joined_text = joined_text .. '\n'
-        end
-
-        joined_text = joined_text .. line
+    for line_number = 1, self.buffer:get_size() do
+        joined_text =
+            joined_text .. self.buffer:get_element_at(line_number) .. '\n'
     end
 
-    return joined_text
+    return joined_text:sub(1, -2)
 end
 
 function TextBuffer:append(text)
-    for line in (text .. '\n'):gmatch'([^\n]*)\n' do
-        self.lines[self.last_line_number] =
-            self.lines[self.last_line_number] .. line
+    local buffer = self.buffer
 
-        self.last_line_number = self.last_line_number + 1
-        self.lines[self.last_line_number] = ''
+    for line in (text .. '\n'):gmatch'([^\n]*)\n' do
+        buffer:set_element_at(buffer:get_size(), buffer:get_element_at(buffer:get_size()) .. line)
+        buffer:push''
     end
 
-    self:backspace()
+    self.buffer:pop()
 end
 
 function TextBuffer:backspace()
-    local line = self.lines[self.last_line_number]
+    local buffer = self.buffer
+    local line = buffer:get_element_at(self.buffer:get_size())
 
     if utf8.len(line) > 0 then
-        self.lines[self.last_line_number] = line:sub(1, utf8.offset(line, -1) - 1)
-    elseif self.last_line_number > 1 then
-        self.lines[self.last_line_number] = nil
-        self.last_line_number = self.last_line_number - 1
+        buffer:set_element_at(buffer:get_size(),
+            line:sub(1, utf8.offset(line, -1) - 1))
+    elseif buffer:get_size() > 1 then
+        self.buffer:pop()
     end
 end
 
 function TextBuffer:clear()
-    self.lines = {''}
-    self.last_line_number = 1
+    self.buffer = RingBuffer:new(self.scrollback_limit)
+    self.buffer:push''
 end
 
 return augment(TextBuffer)
